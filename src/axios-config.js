0de6886765eb612a77d54bbcd12df7815f450528
Joao Antunes <antunesjoao81@gmail.com>
2025-05-05 17:36:20 -0400

    add /memories no lugar certo

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 //base da url (onde fica o servidor)
-axios.defaults.baseURL = "https://fullstack-production-feea.up.railway.app"
+axios.defaults.baseURL = "https://fullstack-production-feea.up.railway.app/memories"
 
 
 //quanto tempo o axios vai levar pra desistir da requisição (10segundos)
