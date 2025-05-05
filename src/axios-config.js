fbe80c3eef22b0c51e6e8b7002f5bb32c209fc4d
Joao Antunes <antunesjoao81@gmail.com>
2025-05-05 17:12:52 -0400

    tirando o /memories do axios

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 //base da url (onde fica o servidor)
-axios.defaults.baseURL = "https://fullstack-production-feea.up.railway.app/memories"
+axios.defaults.baseURL = "https://fullstack-production-feea.up.railway.app"
 
 
 //quanto tempo o axios vai levar pra desistir da requisição (10segundos)
